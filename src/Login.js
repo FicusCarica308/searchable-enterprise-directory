b81cc3241d2e139956220a93e14cd25acc5fe26b
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-11-03 23:14:39 +0000

    added login feature

---
+++ src/Login.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import './Login.css';
+
+function Login(props) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  // ADD a 500 error handle after implementation on server side
+  const handleLogin = () => {
+    fetch('http://localhost:5555/api/employees/user', {
+      method: 'POST',
+      body: JSON.stringify({email, password}),
+      headers: {
+        'Content-Type':'application/json',
+      },
+    })
+    .then((response) => {
+      if (response.ok) {
+        return(response.json());
+      } else {
+        alert('Invalid email or password !')
+        throw Error(response.json());
+      }
+    })
+    .then((data) => {
+      alert('Login Success!');
+      props.setUserData(data);
+      props.setLoggedIn(true);
+    })
+    .catch((error) => console.log('error handled', error));
+  }
+  return (
+    <div id='login'>
+      <form id='login_form' onSubmit={handleSubmit}>
+        <label htmlFor='email_input' id='email_label'>Email:</label>
+        <input className='input' type='email' name='email_input' id='email_input' onChange={(e)=> {setEmail(e.target.value)}}></input>
+        <label htmlFor='password_input' id='password_label'>Password:</label>
+        <input className='input' type='password' name='password_input' id='password_input' onChange={(e)=> {setPassword(e.target.value)}}></input>
+        <button onClick={() => {handleLogin()}}>Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
